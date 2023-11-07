ec231e077283d796064ffd1eaf2ba95b9b3d76f5
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-07 09:15:35 -0400

    featured: delete tutor useCase

---
+++ src/useCases/Tutor/delete.ts
@@ -0,0 +1,22 @@
+import { TutorRepository } from "../../repos/TutorRepo";
+
+export class DeleteTutor {
+    constructor(private repository: TutorRepository) {}
+
+    async execute(tutorId: string) {
+        try {
+            const tutors = await this.repository.listAll();
+            const target = tutors?.find(tutor => tutor.id === tutorId);
+
+            if(target) {
+                await this.repository.delete(tutorId);
+                return `Tutor with ID: ${tutorId} deleted`;
+            } else {
+                throw new Error(`Tutor with ID: ${tutorId}, NOT FOUND!`)
+            }
+
+        } catch(err: any) {
+            throw new Error()
+        }
+    }
+}
