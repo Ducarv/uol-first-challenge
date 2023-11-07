dcb1686107b7077664cee315b979a6c2935117e2
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-07 12:14:46 -0400

    add: delete pet use-case

---
+++ src/useCases/Pet/delete.ts
@@ -0,0 +1,28 @@
+import { PetRepository } from "../../repos/PetRepo";
+
+export class DeletePet {
+    constructor(private repository: PetRepository) {}
+
+    async execute(petId: string, tutorId: string) {
+        try {
+            const pets = await this.repository.listAll();
+
+            if(pets?.length === 0) {
+                throw new Error("Any Pet to delete.");
+            }
+
+            const target = pets?.find(pet => pet.id === petId && pet.tutorId === tutorId);
+
+            if(!target) {
+                throw new Error("Pet not found.");
+            }
+
+            await this.repository.delete(petId, tutorId);
+
+            return `Pet with id: ${petId} deleted.`
+
+        } catch(err:any) {
+            throw new Error("Delete error.");
+        }
+    }
+}
