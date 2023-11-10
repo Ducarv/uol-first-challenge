fd9f6ba0fb148ecda33ec3402302aaf76eb8631a
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-10 16:54:17 -0400

    add/refactor: validate create and update tutor with zod

--- src/controller/Tutor/create.controller.ts
+++ src/controller/Tutor/create.controller.ts
@@ -1,15 +1,32 @@
 import { Request, Response } from 'express';
 import { CreateTutor } from '../../useCases/Tutor/create';
 import { TutorProps } from '../../domain/interfaces/TutorProps';
+import { ValidatorTutorProps } from '../../validators/tutor';
+import { ZodError } from 'zod';
 
 export class CreateTutorController {
   constructor(private createTutorUseCase: CreateTutor) {}
 
   async handle(request: Request, response: Response) {
-    const tutorData: TutorProps = request.body;
+    const tutorData = request.body;
+    let validatedTutorData;
+
+    if (tutorData) {
+      const validationResult = ValidatorTutorProps.safeParse(tutorData);
+
+      if (validationResult.success) {
+        validatedTutorData = validationResult.data;
+      } else {
+        return response
+          .status(400)
+          .send({ errors: validationResult.error.issues });
+      }
+    }
 
     try {
-      const newTutor = await this.createTutorUseCase.execute(tutorData);
+      const newTutor = await this.createTutorUseCase.execute(
+        validatedTutorData as unknown as TutorProps,
+      );
       response.status(201).json(newTutor);
     } catch (err: unknown) {
       response.status(500).json({ error: 'An error occurred' });
--- src/controller/Tutor/update.controller.ts
+++ src/controller/Tutor/update.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { UpdateTutor } from '../../useCases/Tutor/update';
 import { TutorProps } from '../../domain/interfaces/TutorProps';
+import { ValidatorTutorProps } from '../../validators/tutor';
 
 export class UpdateTutorController {
   constructor(private updateTutorUseCase: UpdateTutor) {}
@@ -10,9 +11,18 @@ export class UpdateTutorController {
       const { id } = request.params;
       const updateTutorData: Partial<TutorProps> = request.body;
 
+      const partialValidator = ValidatorTutorProps.partial();
+      const validationResult = partialValidator.safeParse(updateTutorData);
+
+      if (!validationResult.success) {
+        return response
+          .status(400)
+          .json({ errors: validationResult.error.errors });
+      }
+
       const updatedTutor = await this.updateTutorUseCase.execute(
         id,
-        updateTutorData,
+        validationResult.data as TutorProps,
       );
       response.status(202).json(updatedTutor);
     } catch (err: unknown) {
