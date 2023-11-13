759d279525b21d1ebfbf8b3af3566bb67cabe09f
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-13 11:56:45 -0400

    refactor: make dinamic swagger url port

--- src/routes/swagger/swaggerRouter.ts
+++ src/routes/swagger/swaggerRouter.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { default as swaggerDocument } from '../../swagger.json';
+import 'dotenv/config';
 
 export const swaggerRouter = express.Router();
 
 swaggerRouter.use(
   '/api/v1/api-docs',
   swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument),
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      servers: [
+        {
+          url: `http://localhost:${process.env.PORT}`,
+        },
+      ],
+    },
+  }),
 );
